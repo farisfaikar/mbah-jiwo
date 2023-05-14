0a75087954bb21801c317db85b035bc16ffb29a0
LAPTOP-7L3PIEIA\Asus <dzakiarrozaq62@gmail.com>
2023-05-14 20:48:32 +0700

    Update 1 (masih default banget)

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -82,6 +82,84 @@
   </header><!-- End Header -->
 
   <main id="main">
+    <!-- ======= Contact Us Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h1>Contact Us</h1>
+        </div>
+
+        <div class="row">
+
+          {{-- <div class="col-lg-5 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>A108 Adam Street, New York, NY 535022</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@example.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+1 5589 55488 55s</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div> --}}
+          <div class="d-flex justify-content-center" >
+            <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="name">First Name</label>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Enter your first name" required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="name">Last name</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Enter your last name" required>
+                  </div>
+                </div>
+                <div class="form-group mt-3 mb-3">
+                  <label for="name">Email</label>
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Enter your email address" required>
+                </div>
+                <label for="selectExample mt-3">Subject</label>
+                <select class="form-select rounded-0 mb-3" aria-label="Default select example">
+                  <option selected>Select subject</option>
+                  <option value="1">Customer Service</option>
+                </select>
+                <div class="form-group mt-3 mb-3">
+                  <label for="name">Subject</label>
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Type your subject" required>
+                </div>
+                <div class="form-group mt-3 mb-3">
+                  <label for="name">Message</label>
+                  <textarea class="form-control" name="message" rows="10" placeholder="Write your massage" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Submit</button></div>
+              </form>
+            </div>            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Contact Us Section -->
+    
 
     {{-- <!-- ======= Breadcrumbs Section ======= -->
     <section class="breadcrumbs">
