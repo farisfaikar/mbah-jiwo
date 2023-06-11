0183beeb86951398cde41ed223b78963ed4816c5
Dzaki Arrozaq <dzakiarrozaq62@students.unnes.ac.id>
2023-06-11 21:42:25 +0700

    ubah landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -104,7 +104,7 @@
                 </div>
             </section><!-- End Services Section -->
 
-            <!-- ======= Portfolio Section ======= -->
+            {{-- <!-- ======= Portfolio Section ======= -->
             <section id="portfolio" class="portfolio">
                 <div class="container" data-aos="fade-up">
 
@@ -264,9 +264,9 @@
                     </div>
 
                 </div>
-            </section><!-- End Portfolio Section -->
+            </section><!-- End Portfolio Section --> --}}
 
-            <!-- ======= F.A.Q Section ======= -->
+            {{-- <!-- ======= F.A.Q Section ======= -->
             <section id="faq" class="faq section-bg">
                 <div class="container" data-aos="fade-up">
 
@@ -367,13 +367,13 @@
                     </ul>
 
                 </div>
-            </section><!-- End F.A.Q Section -->
+            </section><!-- End F.A.Q Section --> --}}
 
             <!-- ======= Team Section ======= -->
             <section id="team" class="team">
                 <div class="container">
 
-                    <div class="section-title" data-aos="fade-up">
+                    <div class="section-titlee" data-aos="fade-up">
                         <h2>Team</h2>
                         <p>Our team is always here to help</p>
                     </div>
@@ -461,7 +461,7 @@
             <section id="clients" class="clients section-bg">
                 <div class="container" data-aos="fade-up">
 
-                    <div class="section-title">
+                    <div class="section-titlee">
                         <h2>Clients</h2>
                         <p>They trusted us</p>
                     </div>
@@ -495,7 +495,7 @@
             <section id="contact" class="contact">
                 <div class="container" data-aos="fade-up">
 
-                    <div class="section-title">
+                    <div class="section-titlee">
                         <h2>Contact Us</h2>
                         <p>Contact us the get started</p>
                     </div>
@@ -507,25 +507,26 @@
                                 <div class="address">
                                     <i class="bi bi-geo-alt"></i>
                                     <h4>Location:</h4>
-                                    <p>A108 Adam Street, New York, NY 535022</p>
+                                    <p>Kampus Sekaran Gunungpati, Kota Semarang, 50229, Jawa Tengah</p>
                                 </div>
 
                                 <div class="email">
                                     <i class="bi bi-envelope"></i>
                                     <h4>Email:</h4>
-                                    <p>info@example.com</p>
+                                    <p>farisfaikar.r@gmail.com</p>
                                 </div>
 
                                 <div class="phone">
                                     <i class="bi bi-phone"></i>
                                     <h4>Call:</h4>
-                                    <p>+1 5589 55488 55s</p>
+                                    <p>+62 896-4645-8649s</p>
                                 </div>
 
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
                                     frameborder="0" style="border:0; width: 100%; height: 290px;"
-                                    allowfullscreen></iframe>
+                                    allowfullscreen>
+                                </iframe>
                             </div>
 
                         </div>
@@ -534,32 +535,34 @@
                             data-aos-delay="200">
                             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
                                 <div class="row">
-                                    <div class="form-group col-md-6">
-                                        <label for="name">Your Name</label>
-                                        <input type="text" name="name" class="form-control" id="name"
-                                            placeholder="Your Name" required>
+                                    <div class="form-group">
+                                      <label for="nama">Nama</label>
+                                      <input type="text" name="nama" class="form-control" id="nama" placeholder="Masukkan nama" required>
                                     </div>
-                                    <div class="form-group col-md-6 mt-3 mt-md-0">
-                                        <label for="name">Your Email</label>
-                                        <input type="email" class="form-control" name="email" id="email"
-                                            placeholder="Your Email" required>
-                                    </div>
-                                </div>
-                                <div class="form-group mt-3">
-                                    <label for="name">Subject</label>
-                                    <input type="text" class="form-control" name="subject" id="subject"
-                                        placeholder="Subject" required>
-                                </div>
-                                <div class="form-group mt-3">
-                                    <label for="name">Message</label>
-                                    <textarea class="form-control" name="message" rows="10" required></textarea>
-                                </div>
-                                <div class="my-3">
+                                  </div>
+                                  <div class="form-group mt-3 mb-3">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="Masukkan email" required>
+                                  </div>
+                                  <label for="perihal">Perihal</label>
+                                  <select class="form-select rounded-0 mb-3" aria-label="Default select example" name="perihal" required>
+                                    <option value="Pelayanan Pelanggan" selected>Pelayanan Pelanggan</option>
+                                    <option value="Umpan Balik">Umpan Balik</option>
+                                  </select>
+                                  <div class="form-group mt-3 mb-3">
+                                    <label for="judul">Judul</label>
+                                    <input type="text" class="form-control" name="judul" id="judul" placeholder="Masukkan judul" required>
+                                  </div>
+                                  <div class="form-group mt-3 mb-3">
+                                    <label for="pesan">Pesan</label>
+                                    <textarea class="form-control" name="pesan" rows="10" placeholder="Tulis pesan" required></textarea>
+                                  </div>
+                                  <div class="my-3">
                                     <div class="loading">Loading</div>
                                     <div class="error-message"></div>
                                     <div class="sent-message">Your message has been sent. Thank you!</div>
-                                </div>
-                                <div class="text-center"><button type="submit">Send Message</button></div>
+                                  </div>
+                                  <div class="text-center"><button type="submit">Submit</button></div>
                             </form>
                         </div>
 
